d924bbb96cdeab26b1a70ccdba2ed1036ca5af53
kremenevskij <v.kremenevskij@cft.ru>
2022-12-17 22:38:03 +0300

    add data to transaction type table and for action table

---
+++ Database/tables_insertion.sql
@@ -0,0 +1,8 @@
+insert into actions (action_name) VALUES ('home_button_pressed');
+insert into actions (action_name) VALUES ('back_button_pressed');
+insert into actions (action_name) VALUES ('card_addition_button_pressed');
+insert into actions (action_name) VALUES ('send_transaction_button_pressed');
+
+
+insert into trans_type (type_name, started_from) VALUES ('CRD2CSH', '2022-12-17');
+insert into trans_type (type_name, started_from) VALUES ('CRD2BANK', '2022-12-17');
